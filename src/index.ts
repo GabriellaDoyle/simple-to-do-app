a064d6ec27256a0681822b964ee16de870a9fa16
Gabriella Doyle <Gabrielladoyle2001@gmail.com>
2023-06-30 11:05:27 +0100

    fixing types

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,34 @@
-import { v4 as uuidV4 } from 'uuid'
+import { Task } from "../types/types";
+import { v4 as uuidV4 } from "uuid";
 
-console.log(uuidV4())
-
-const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
-const task = []
-interface Task {
-  id: string;
-  title: string;
-  completed: boolean;
-  createdAt: Date;
-}
+const tasks = []
 
-form?.addEventListener("submit", e => {
-  e.preventDefault()
-  if (input?.value == "" || input?.value == null) return
-  
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
+
   const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
-    createdAt: new Date()
-  }
+    created: new Date(),
+  };
 
-  addListItem(newTask)
-})
+  addListItem(newTask);
+  input.value = "";
+});
 
-function addListItem(newTask:Task): boolean{
-  const item = document.createElement("li")
-  const label = document.createElement("label")
-  const checkbox = document.createElement("input")
-  checkbox.type = "checkbox"
-  checkbox.checked = newTask.completed
-  label.append(checkbox, newTask.title)
-  item.append(label)
-  list?.append(item)
-  return true
-}
-
-
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const addListItem = (task: Task) => {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+};
