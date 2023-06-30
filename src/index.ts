9e6a1889c495bceb6d549309c3a1df88e7c3e7c3
Gabriella Doyle <Gabrielladoyle2001@gmail.com>
2023-06-30 10:49:29 +0100

    change to snowpack

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid'
+
+console.log(uuidV4())
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("#new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const task = []
+interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+}
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+  if (input?.value == "" || input?.value == null) return
+  
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  addListItem(newTask)
+})
+
+function addListItem(newTask:Task): boolean{
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.checked = newTask.completed
+  label.append(checkbox, newTask.title)
+  item.append(label)
+  list?.append(item)
+  return true
+}
+
+
 
 import confetti from 'canvas-confetti';
 
